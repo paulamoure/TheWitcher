4770d0a576b1d23cfa05f24199f6cb4c1be6733b
Paula <paula@gmail.com>
2023-01-11 12:47:27 +0100

    added getters + setters

--- Eclipse/TheWitcher/src/Witcher.java
+++ Eclipse/TheWitcher/src/Witcher.java
@@ -1,8 +1,19 @@
 
 public class Witcher {
+	private String name;
+	private int health;
+	private boolean hasSoul;
 
 	public static void main(String[] args) {
 		
+	}
+
+	private String getName() {
+		return name;
+	}
+
+	private void setName(String name) {
+		this.name = name;
 	}
 
 }
